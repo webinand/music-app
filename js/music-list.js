a2756505ca30239a902084a7913e1b03aa55bff9
webinand <OGARFERDINAND690@GMAIL.COM>
2023-06-09 08:21:37 +0100

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,74 @@
+let allMusic = [
+  {
+    name: " Blood sweat & tears",
+    artist: "BTS",
+    img: "music-1",
+    src: "music-1",
+  },
+  {
+    name: "playing with fire",
+    artist: "Black Pink",
+    img: "music-2",
+    src: "music-2",
+  },
+  {
+    name: "spring day",
+    artist: "BTS",
+    img: "music-3",
+    src: "music-3",
+  },
+  {
+    name: "kiss and make up",
+    artist: "Black Pink",
+    img: "music-4",
+    src: "music-4",
+  },
+  {
+    name: "ON",
+    artist: "BTS",
+    img: "music-5",
+    src: "music-5",
+  },
+  {
+    name: "whistle",
+    artist: "Black Pink",
+    img: "music-6",
+    src: "music-6",
+  },
+  {
+    name: "dynamite",
+    artist: "BTS",
+    img: "music-7",
+    src: "music-7",
+  },
+  {
+    name: "ddu du ddu du",
+    artist: "Black Pink",
+    img: "music-8",
+    src: "music-8",
+  },
+  {
+    name: "butter",
+    artist: "BTS",
+    img: "music-9",
+    src: "music-9",
+  },
+  {
+    name: "Lovesick girls",
+    artist: "Black Pink",
+    img: "music-10",
+    src: "music-10",
+  },
+  {
+    name: "Black swan",
+    artist: "BTS",
+    img: "music-11",
+    src: "music-11",
+  },
+  {
+    name: "Kill this love",
+    artist: "Black Pink",
+    img: "music-12",
+    src: "music-12",
+  },
+];
